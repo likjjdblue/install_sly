9a25cc262b6e73b1f788be9e3e2dd987c9721c4f
dblue <likjjdblue@gmail.com>
2021-01-20 10:04:12 +0800

    fix  & update

---
+++ apps/caibian/ddc/metadata.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+AppInfo = {
+    'AppName': 'ddc',
+    'NFSAddr': None,
+    'NFSBasePath': None,
+    'DDCLogPath': None,
+    'Namespace': None,
+    'TargetNamespaceDIR': '../../.././namespaces',
+    'HarborAddr': None,
+    'DDCImage': 'registry.cn-hangzhou.aliyuncs.com/trssly/ddc:v1',
+    "NFSProvisionerImage": 'quay.io/external_storage/nfs-client-provisioner:latest',
+    'DDCDBHost': 'mariadb-svc',
+    'DDCDBName': 'mty_ddc',
+    'DDCDBUser': 'mtyddc',
+    "DDCDBPassword": None,
+    'DDCRedisHost': 'redis-svc',
+    'DDCRedisPort': '6379',
+    'DDCRedisPassword': None,
+    'DDCMQUser': 'admin',
+    'DDCMQPassword': None,
+    'dependences': ['apps.common.mariadb/MariaDBTool', 'apps.common.redisha/RedisHATool'
+                    'apps.common.rabbitmqha/RabbitmqHATool', 'apps.caibian.trwcm/TRSWCMTool',
+                    ]
+}
