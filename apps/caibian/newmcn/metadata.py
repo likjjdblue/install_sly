bc5565883f3631ad9c02dd9ed12e5258c0a8e1f0
dblue <likjjdblue@gmail.com>
2021-01-21 00:28:33 +0800

    fix  & update

---
+++ apps/caibian/newmcn/metadata.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+AppInfo = {
+    'AppName': 'newmcn',
+    'NFSAddr': None,
+    'NFSBasePath': None,
+    'MCNDataPath': None,
+    'MCNLogPath': None,
+    'Namespace': None,
+    'TargetNamespaceDIR': '../../.././namespaces',
+    'HarborAddr': None,
+    'MCNImage': 'registry.cn-hangzhou.aliyuncs.com/trssly/mcn:v1',
+    "NFSProvisionerImage": 'quay.io/external_storage/nfs-client-provisioner:latest',
+    'UPCDBHost': 'mariadb-svc',
+    'UPCDBName': 'mcn_upc',
+    'UPCDBUser': 'mcnupc',
+    "UPCDBPassword": None,
+
+    'UPC_MtyDBHost': 'mariadb-svc',
+    'UPC_MtyDBName': 'mcn_upc_mty',
+    'UPC_MtyDBUser': 'mcnupc',
+    "UPC_MtyDBPassword": None,
+
+    'UPC_QuartzDBHost': 'mariadb-svc',
+    'UPC_QuartzDBName': 'mcn_upc_quartz',
+    'UPC_QuartzDBUser': 'mcnupc',
+    "UPC_QuartzDBPassword": None,
+
+    'UPC_MrsDBHost': 'mariadb-svc',
+    'UPC_MrsDBName': 'mty_mrs',
+    'UPC_MrsDBUser': 'mcnupc',
+    "UPC_MrsDBPassword": None,
+
+    'UPCRedisHost': 'redis-svc',
+    'UPCRedisPort': '6379',
+    'UPCRedisPassword': None,
+
+    'dependences': ['apps.common.mariadb/MariaDBTool', 'apps.common.nginx/NginxTool',
+                    'apps.common.redis/RedisTool'
+                    ]
+}
