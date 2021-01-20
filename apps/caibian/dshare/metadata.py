1bb508479a00651929c5aa3d797a4c28bea1d7a2
dblue <likjjdblue@gmail.com>
2021-01-20 22:31:27 +0800

    fix  & update

---
+++ apps/caibian/dshare/metadata.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+AppInfo = {
+    'AppName': 'dshare',
+    'NFSAddr': None,
+    'NFSBasePath': None,
+    'DshareLogPath': None,
+    'Namespace': None,
+    'TargetNamespaceDIR': '../../.././namespaces',
+    'HarborAddr': None,
+    'DshareImage': 'registry.cn-hangzhou.aliyuncs.com/trssly/dshare:v1',
+    "NFSProvisionerImage": 'quay.io/external_storage/nfs-client-provisioner:latest',
+    'DshareDBHost': 'mariadb-svc',
+    'DshareDBName': 'mty_dshare',
+    'DshareDBUser': 'dshare',
+    "DshareDBPassword": None,
+    'dependences': ['apps.common.mariadb/MariaDBTool', 'apps.common.nginx/NginxTool',
+                    ]
+}
