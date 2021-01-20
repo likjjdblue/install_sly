6181a55221e343751eeaf24a71bf51e3cc952fde
dblue <likjjdblue@gmail.com>
2021-01-20 21:04:09 +0800

    fix  & update

---
+++ apps/caibian/downimg/metadata.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+AppInfo = {
+    'AppName': 'downimg',
+    'NFSAddr': None,
+    'NFSBasePath': None,
+    'DownImgDataPath': None,
+    'DownImgLogPath': None,
+    'Namespace': None,
+    'TargetNamespaceDIR': '../../.././namespaces',
+    'HarborAddr': None,
+    'DownImgImage': 'registry.cn-hangzhou.aliyuncs.com/trssly/tmyimgcenter:v1',
+    "NFSProvisionerImage": 'quay.io/external_storage/nfs-client-provisioner:latest',
+    'DownImgDBHost': 'mariadb-svc',
+    'DownImgDBName': 'devdc',
+    'DownImgDBUser': 'tmyimgcenter',
+    "DownImgDBPassword": None,
+    'dependences': ['apps.common.mariadb/MariaDBTool', 'apps.common.nginx/NginxTool',
+                    ]
+}
