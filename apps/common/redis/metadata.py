cb753aebff1c1782d65601fa4fa7a1f610684c82
dblue <likjjdblue@gmail.com>
2021-01-20 13:12:21 +0800

    fix  & update

---
+++ apps/common/redis/metadata.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+AppInfo = {
+    'AppName': 'redis',
+    'NFSAddr': None,
+    'NFSBasePath': None,
+    'RedisDataPath': None,
+    'RedisStandAlonePassword': None,
+    'Namespace': None,
+    'TargetNamespaceDIR': '../../.././namespaces',
+    'HarborAddr': None,
+    'RedisImage': 'redis:5.0',
+    "NFSProvisionerImage": 'quay.io/external_storage/nfs-client-provisioner:latest',
+}
