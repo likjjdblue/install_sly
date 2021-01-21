739e43546820d77be04c24fbbcf05df1d9735476
dblue <likjjdblue@gmail.com>
2021-01-21 11:30:48 +0800

    fix  & update

---
+++ apps/caibian/paperreview/metadata.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+AppInfo = {
+    'AppName': 'paperreview',
+    'NFSAddr': None,
+    'NFSBasePath': None,
+    'PaperReviewLogPath': None,
+    'Namespace': None,
+    'TargetNamespaceDIR': '../../.././namespaces',
+    'HarborAddr': None,
+    'PaperReviewImage': 'registry.cn-hangzhou.aliyuncs.com/trssly/paperreview:v1',
+    "NFSProvisionerImage": 'quay.io/external_storage/nfs-client-provisioner:latest',
+    'PaperReviewDBHost': 'mariadb-svc',
+    'PaperReviewDBName': 'mty_wcm',
+    'PaperReviewDBUser': 'mtywcm',
+    "PaperReviewDBPassword": None,
+    'PaperReviewRedisHost': 'redis-svc',
+    'PaperReviewRedisPort': '6379',
+    'PaperReviewRedisPassword': None,
+    'PaperReviewNacosAddr': 'nacos-svc',
+    'PaperReviewNacosID': '8953ca45-4cfb-469a-bc55-7e3bce569994',
+    'dependences': ['apps.common.mariadb/MariaDBTool', 'apps.common.redis/RedisTool',
+                    'apps.common.rabbitmqha/RabbitmqHATool', 'apps.common.nacos/NacosTool',
+                    'apps.caibian.trswcm/TRSWCMTool',
+                    ]
+}
