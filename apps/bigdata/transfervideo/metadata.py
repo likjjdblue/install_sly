30e1e55aa7992372268c3a625efd33a54ede042d
dblue <likjjdblue@gmail.com>
2021-01-25 13:28:24 +0800

    fix  & update

---
+++ apps/bigdata/transfervideo/metadata.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+AppInfo = {
+    'AppName': 'transfervideo',
+    'NFSAddr': None,
+    'NFSBasePath': None,
+    'TransferVideoLogPath': None,
+    'Namespace': None,
+    'TargetNamespaceDIR': '../../.././namespaces',
+    'HarborAddr': None,
+    'TransferVideoImage': 'registry.cn-hangzhou.aliyuncs.com/trssly/transfer_video:v1',
+    "NFSProvisionerImage": 'quay.io/external_storage/nfs-client-provisioner:latest',
+
+    'TransferVideoPrimaryDBHost': 'mariadb-svc',
+    'TransferVideoPrimaryDBName': 'transfer',
+    'TransferVideoPrimaryDBUser': 'transfer',
+    "TransferVideoPrimaryDBPassword": None,
+
+    'RedisHAHost': 'redis-ha-announce-0:26379,redis-ha-announce-1:26379,redis-ha-announce-2:26379',
+    'RedisHAMasterName': 'mymaster',
+    'RedisHAPassword': None,
+
+    'dependences': ['apps.common.mariadb/MariaDBTool', 'apps.common.nginx/NginxTool',
+                    'apps.common.redisha/RedisHATool', 'apps.common.nacos/NacosTool',
+                     'apps.common.zookeeper/ZookeeperTool','apps.common.kafka/KafkaTool',
+                    'apps.bigdata.transferresourceai/TransferResourceAITool',
+                    ]
+}
