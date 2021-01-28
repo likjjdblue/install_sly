69b8f4bd51bbae8dc1dcea7a282545972361ff3b
dblue <likjjdblue@gmail.com>
2021-01-28 09:43:26 +0800

    fix  & update

---
+++ apps/caibian/newmediaplatform/metadata.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+AppInfo = {
+    'AppName': 'newmediaplatform',
+    'NFSAddr': None,
+    'NFSBasePath': None,
+    'NewMediaPlatformLogPath': None,
+    'NewMediaPlatformDataPath': None,
+    'Namespace': None,
+    'TargetNamespaceDIR': '../../.././namespaces',
+    'HarborAddr': None,
+    'NewMediaPlatformImage': 'registry.cn-hangzhou.aliyuncs.com/trssly/newmediaplatform:v1',
+    "NFSProvisionerImage": 'quay.io/external_storage/nfs-client-provisioner:latest',
+    'NewMediaPlatformDBHost': 'mariadb-svc',
+    'NewMediaPlatformDBName': 'new_media_platform',
+    'NewMediaPlatformDBUser': 'newmedia',
+    "NewMediaPlatformDBPassword": None,
+
+    'TRSRedisHost': 'redis-svc',
+    'TRSRedisPort': '6379',
+    'TRSRedisPassword': None,
+
+    'RedisPassword': None,
+    'dependences': ['apps.common.mariadb/MariaDBTool', 'apps.common.nginx/NginxTool',
+                    'apps.common.redis/RedisTool',
+                    ]
+}
