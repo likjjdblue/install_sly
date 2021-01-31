057507292c76217c8d482cedb085c91a7e410ca7
dblue <likjjdblue@gmail.com>
2021-01-31 14:06:39 +0800

    fix & update

--- apps/caibian/newmcnadmin/newmcnadmin.py
+++ apps/caibian/newmcnadmin/newmcnadmin.py
@@ -3,7 +3,7 @@
 
 import sys, os
 sys.path.append('../../..')
-from apps.common import nfs, nfsprovisioner, sqltool
+from apps.common import nfs, nfsprovisioner, sqltool, servicestatecheck
 from tools import k8s_tools, ssh_tools
 from metadata import AppInfo
 from copy import deepcopy
@@ -20,7 +20,7 @@ import importlib
 
 class MCNAdminTool(object):
     def __init__(self, namespace='default', mcnadmindatapath='mcn-pv-data/MCNData', mcnadminlogdata='mcnadmin-pv-log' ,
-                 nfsinfo={}, harbor=None, retrytimes=10, *args, **kwargs):
+                 nfsinfo={}, harbor=None, retrytimes=60, *args, **kwargs):
 
         namespace = namespace.strip()
         kwargs['namespace'] = namespace
@@ -191,14 +191,20 @@ class MCNAdminTool(object):
             TmpResponse = self.k8sObj.getNamespacedDeployment(name='mcnadmin-deploy',
                                                                    namespace=self.AppInfo['Namespace'])['result'].to_dict()
 
-            if (TmpResponse['status']['replicas'] != TmpResponse['status']['ready_replicas']) and \
-                   (TmpResponse['status']['replicas'] is not None):
+            if not self.k8sObj.checkNamespacedResourceHealth(name='mcnadmin-deploy', namespace=self.AppInfo['Namespace'],
+                                                         kind='Deployment'):
                 print ('Waitting for Deployment  %s to be ready,replicas: %s, available replicas: %s')%(
                     TmpResponse['metadata']['name'], str(TmpResponse['status']['replicas']),
                     str(TmpResponse['status']['ready_replicas'])
                 )
                 sleep (20)
                 continue
+
+            TmpServiceCheckObj = servicestatecheck.ServiceStateCheckTool(namespace=self.AppInfo['Namespace'],
+                                                                         harbor=self.AppInfo['HarborAddr'])
+            TmpCheckResult = TmpServiceCheckObj.checkServicePortState(targetaddress='mcnadmin-svc:9002')
+            print ('mcnadmin-svc:9002 is listening....')
+
             print ('Deployment: %s is available;replicas: %s')%(TmpResponse['metadata']['name'],
                                                               str(TmpResponse['status']['replicas']))
             isRunning = True
@@ -210,8 +216,7 @@ class MCNAdminTool(object):
                 'ret_code': 1,
                 'result': 'Failed to apply Deployment: %s'%(TmpResponse['metadata']['name'],)
             }
-        print ('Waitting TRS MCN Admin for running....')
-        sleep(120)
+
 
         return {
             'ret_code': 0,
@@ -243,6 +248,7 @@ class MCNAdminTool(object):
         print ('install %s successsfully')%(self.AppInfo['AppName'], )
         print ('configging Nginx for %s')%(self.AppInfo['AppName'])
         self.postInstall()
+        self.close()
 
         return {
             'ret_code': 0,
@@ -389,6 +395,11 @@ class MCNAdminTool(object):
             with open(os.path.join(TmpTargetNamespaceDIR, 'values.yaml'), mode='rb') as f:
                 TmpValuse = yaml.safe_load(f)
         return TmpValuse
+
+
+
+    def close(self):
+        self.NFSObj.close()
 
 
 
