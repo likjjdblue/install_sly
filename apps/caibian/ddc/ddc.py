4647246d6cf69c1b560bce2ada07ecf814fdbb29
dblue <likjjdblue@gmail.com>
2021-02-04 10:43:42 +0800

    fix & update

--- apps/caibian/ddc/ddc.py
+++ apps/caibian/ddc/ddc.py
@@ -183,8 +183,8 @@ class DDCTool(object):
 
             TmpServiceCheckObj = servicestatecheck.ServiceStateCheckTool(namespace=self.AppInfo['Namespace'],
                                                                          harbor=self.AppInfo['HarborAddr'])
-            TmpCheckResult = TmpServiceCheckObj.checkServicePortState(targetaddress='nginx-svc:80')
-            print ('ddc-svc:9016 is listening....')
+            TmpCheckResult = TmpServiceCheckObj.checkServicePortState(targetaddress='ddc-svc:9998')
+            print ('ddc-svc:9998 is listening....')
 
             print ('Deployment: %s is available;replicas: %s')%(TmpResponse['metadata']['name'],
                                                               str(TmpResponse['status']['replicas']))
