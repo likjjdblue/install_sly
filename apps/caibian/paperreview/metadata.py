52a68201fde1ee0d69ec6ac0b16fec5d3c489d27
dblue <likjjdblue@gmail.com>
2021-02-26 17:14:36 +0800

    fix & update

--- apps/caibian/paperreview/metadata.py
+++ apps/caibian/paperreview/metadata.py
@@ -3,8 +3,12 @@
 
 AppInfo = {
     'AppName': 'paperreview',
-    'NFSAddr': None,
-    'NFSBasePath': None,
+
+    'DataStorageAddr': None,
+    'DataStorageBasePath': None,
+    'LogStorageAddr': None,
+    'LogStorageBasePath': None,
+
     'PaperReviewLogPath': None,
     'Namespace': None,
     'TargetNamespaceDIR': '../../.././namespaces',
