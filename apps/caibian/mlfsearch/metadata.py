f80328426b7b0133786083334605091ac11f8a38
dblue <likjjdblue@gmail.com>
2021-02-26 11:09:20 +0800

    fix & update

--- apps/caibian/mlfsearch/metadata.py
+++ apps/caibian/mlfsearch/metadata.py
@@ -3,8 +3,12 @@
 
 AppInfo = {
     'AppName': 'mlfsearch',
-    'NFSAddr': None,
-    'NFSBasePath': None,
+
+    'DataStorageAddr': None,
+    'DataStorageBasePath': None,
+    'LogStorageAddr': None,
+    'LogStorageBasePath': None,
+
     'MLFSearchLogPath': None,
     'Namespace': None,
     'TargetNamespaceDIR': '../../.././namespaces',
--- apps/caibian/mlfsearch/mlfsearch.py
+++ apps/caibian/mlfsearch/mlfsearch.py
@@ -3,7 +3,7 @@
 
 import sys, os
 sys.path.append('../../..')
-from apps.common import nfs, nfsprovisioner, sqltool, servicestatecheck
+from apps.common import nfsprovisioner, sqltool, servicestatecheck
 from tools import k8s_tools, ssh_tools
 from metadata import AppInfo
 from copy import deepcopy
@@ -17,36 +17,38 @@ from tools import k8s_tools
 from pprint import pprint
 from codecs import open as open
 import importlib
+from storagenode import datastoragenode, logstoragenode
+from apps.storage import getClsObj
+
+
 
 class MLFSearchTool(object):
-    def __init__(self, namespace='default', mlfsearchlogdata='mlfsearch-pv-log' ,
-                 nfsinfo={}, harbor=None, retrytimes=60, *args, **kwargs):
+    def __init__(self, namespace='default', mlfsearchlogdata='mlfsearch-pv-log',
+                 harbor=None, retrytimes=60, *args, **kwargs):
 
         namespace = namespace.strip()
         kwargs['namespace'] = namespace
-        kwargs['nfsinfo'] = nfsinfo
         kwargs['harbor'] = harbor
         self.kwargs = kwargs
 
         self.RetryTimes = int(retrytimes)
-        self.NFSAddr = nfsinfo['hostname']
-        self.NFSPort = nfsinfo['port']
-        self.NFSUsername = nfsinfo['username']
-        self.NFSPassword = nfsinfo['password']
-        self.NFSBasePath = nfsinfo['basepath']
         self.AppInfo = deepcopy(AppInfo)
 
-        self.AppInfo['NFSAddr'] = self.NFSAddr
-        self.AppInfo['NFSBasePath'] = self.NFSBasePath
+        self.AppInfo['DataStorageAddr'] = datastoragenode['hostname']
+        self.AppInfo['DataStorageBasePath'] = datastoragenode['basepath']
+        self.AppInfo['LogStorageAddr'] = logstoragenode['hostname']
+        self.AppInfo['LogStorageBasePath'] = logstoragenode['basepath']
 
-        self.AppInfo['MLFSearchLogPath'] = os.path.join(self.AppInfo['NFSBasePath'], '-'.join([namespace, mlfsearchlogdata]))
+
+
+        self.AppInfo['MLFSearchLogPath'] = os.path.join(self.AppInfo['LogStorageBasePath'], '-'.join([namespace, mlfsearchlogdata]))
         self.AppInfo['Namespace'] = namespace
 
         self.AppInfo['HarborAddr'] = harbor
         self.k8sObj = k8s_tools.K8SClient()
 
-        self.NFSObj = nfs.NFSTool(**nfsinfo)
-        self.SSHClient = ssh_tools.SSHTool(**nfsinfo)
+        self.DataStorageObj = getClsObj(datastoragenode['type'])(**datastoragenode)
+        self.LogStorageObj = getClsObj(logstoragenode['type'])(**logstoragenode)
 
         self.DependencyDict ={}
         TmpCWDPath = os.path.abspath(__file__)
@@ -57,16 +59,22 @@ class MLFSearchTool(object):
             print ('load from file....')
             self.AppInfo = deepcopy(self.getValues())
 
-    def setupNFS(self):
-        TmpResponse = self.NFSObj.installNFS(basedir=self.AppInfo['NFSBasePath'])
+    def setupStorage(self):
+        TmpResponse = self.DataStorageObj.installStorage(basedir=self.AppInfo['DataStorageBasePath'])
         if TmpResponse['ret_code'] != 0:
             return TmpResponse
 
-        print ('create MLFSearch NFS successfully')
 
-        self.NFSObj.createSubFolder(self.AppInfo['MLFSearchLogPath'])
+        TmpResponse = self.LogStorageObj.installStorage(basedir=self.AppInfo['LogStorageBasePath'])
+        if TmpResponse['ret_code'] != 0:
+            return TmpResponse
 
-        print ('setup MLFSearch NFS successfully')
+
+        print ('create MLFSearch Storage successfully')
+
+        self.LogStorageObj.createSubFolder(self.AppInfo['MLFSearchLogPath'])
+
+        print ('setup MLFSearch Storage successfully')
 
         return {
             'ret_code': 0,
@@ -209,7 +217,7 @@ class MLFSearchTool(object):
         }
 
     def start(self):
-        TmpResponse = self.setupNFS()
+        TmpResponse = self.setupStorage()
         if TmpResponse['ret_code'] != 0:
             return TmpResponse
 
@@ -298,22 +306,22 @@ class MLFSearchTool(object):
         print (TmpSQLToolAccountPath)
         print (TmpSQLToolSQLPath)
 
-        self.SSHClient.ExecCmd('mkdir -p %s'%(TmpSQLToolSQLPath, ))
-        self.SSHClient.ExecCmd('mkdir -p %s'%(TmpSQLToolAccountPath, ))
+        self.DataStorageObj.ExecCmd('mkdir -p %s'%(TmpSQLToolSQLPath, ))
+        self.DataStorageObj.ExecCmd('mkdir -p %s'%(TmpSQLToolAccountPath, ))
 
 
 
-        self.SSHClient.ExecCmd('rm -f -r %s/*'%(TmpSQLToolAccountPath, ))
-        self.SSHClient.ExecCmd('rm -f -r %s/*'%(TmpSQLToolSQLPath,)
+        self.DataStorageObj.ExecCmd('rm -f -r %s/*'%(TmpSQLToolAccountPath, ))
+        self.DataStorageObj.ExecCmd('rm -f -r %s/*'%(TmpSQLToolSQLPath,)
 
         print (os.path.join(self.BaseDIRPath, 'tmp', 'account.txt'))
         print (os.path.join(self.BaseDIRPath, 'downloads', 'mty_wcm.sql'))
 
-        self.SSHClient.uploadFile(localpath=os.path.join(self.BaseDIRPath, 'tmp', 'account.txt'),
+        self.DataStorageObj.uploadFile(localpath=os.path.join(self.BaseDIRPath, 'tmp', 'account.txt'),
                                   remotepath=os.path.join(TmpSQLToolAccountPath, 'account.txt')
                                   )
 
-        self.SSHClient.uploadFile(localpath=os.path.join(self.BaseDIRPath, 'downloads', 'mty_wcm.sql'),
+        self.DataStorageObj.uploadFile(localpath=os.path.join(self.BaseDIRPath, 'downloads', 'mty_wcm.sql'),
                                   remotepath=os.path.join(TmpSQLToolSQLPath, 'mty_wcm.sql')
                                   )
 
@@ -335,12 +343,12 @@ class MLFSearchTool(object):
 
     def postInstall(self):
         TmpNginxConfigPath = '-'.join([self.AppInfo['Namespace'], 'nginx-pv-config'])
-        TmpNginxConfigPath = os.path.realpath(os.path.join(self.AppInfo['NFSBasePath'], TmpNginxConfigPath))
+        TmpNginxConfigPath = os.path.realpath(os.path.join(self.AppInfo['DataStorageBasePath'], TmpNginxConfigPath))
 
         print (TmpNginxConfigPath)
-        self.SSHClient.ExecCmd('mkdir -p %s' % (TmpNginxConfigPath, ))
+        self.DataStorageObj.ExecCmd('mkdir -p %s' % (TmpNginxConfigPath, ))
 
-        self.SSHClient.uploadFile(localpath=os.path.join(self.BaseDIRPath, 'downloads', 'mlfsearch.conf'),
+        self.DataStorageObj.uploadFile(localpath=os.path.join(self.BaseDIRPath, 'downloads', 'mlfsearch.conf'),
                                   remotepath=os.path.join(TmpNginxConfigPath, 'mlfsearch.conf')
                                   )
 
@@ -375,8 +383,8 @@ class MLFSearchTool(object):
         return TmpValuse
 
     def close(self):
-        self.NFSObj.close()
-        self.SSHClient.close()
+        self.DataStorageObj.close()
+        self.LogStorageObj.close()
 
 
 
