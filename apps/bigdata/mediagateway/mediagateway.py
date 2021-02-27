581291cd945941586c5ff7ff1c81666294df17a5
dblue <likjjdblue@gmail.com>
2021-02-27 11:45:34 +0800

    fix & update

--- apps/bigdata/mediagateway/mediagateway.py
+++ apps/bigdata/mediagateway/mediagateway.py
@@ -3,7 +3,7 @@
 
 import sys, os
 sys.path.append('../../..')
-from apps.common import nfs, nfsprovisioner, sqltool, servicestatecheck
+from apps.common import nfsprovisioner, sqltool, servicestatecheck
 from tools import k8s_tools, ssh_tools
 from metadata import AppInfo
 from copy import deepcopy
@@ -17,37 +17,36 @@ from tools import k8s_tools
 from pprint import pprint
 from codecs import open as open
 import importlib
+from storagenode import datastoragenode, logstoragenode
+from apps.storage import getClsObj
 
 class MediaGatewayTool(object):
     def __init__(self, namespace='default', mediagatewayklog='media-gateway-pv-log' ,
-                 nfsinfo={}, harbor=None, retrytimes=60, *args, **kwargs):
+                 harbor=None, retrytimes=60, *args, **kwargs):
 
         namespace = namespace.strip()
         kwargs['namespace'] = namespace
-        kwargs['nfsinfo'] = nfsinfo
         kwargs['harbor'] = harbor
         self.kwargs = kwargs
 
         self.RetryTimes = int(retrytimes)
-        self.NFSAddr = nfsinfo['hostname']
-        self.NFSPort = nfsinfo['port']
-        self.NFSUsername = nfsinfo['username']
-        self.NFSPassword = nfsinfo['password']
-        self.NFSBasePath = nfsinfo['basepath']
         self.AppInfo = deepcopy(AppInfo)
 
+        self.AppInfo['DataStorageAddr'] = datastoragenode['hostname']
+        self.AppInfo['DataStorageBasePath'] = datastoragenode['basepath']
+        self.AppInfo['LogStorageAddr'] = logstoragenode['hostname']
+        self.AppInfo['LogStorageBasePath'] = logstoragenode['basepath']
 
-        self.AppInfo['NFSAddr'] = self.NFSAddr
-        self.AppInfo['NFSBasePath'] = self.NFSBasePath
-        self.AppInfo['MediaGatewayLogPath'] = os.path.join(self.AppInfo['NFSBasePath'], '-'.join([namespace, mediagatewayklog]))
+
+        self.AppInfo['MediaGatewayLogPath'] = os.path.join(self.AppInfo['LogStorageBasePath'], '-'.join([namespace, mediagatewayklog]))
 
         self.AppInfo['Namespace'] = namespace
 
         self.AppInfo['HarborAddr'] = harbor
         self.k8sObj = k8s_tools.K8SClient()
 
-        self.NFSObj = nfs.NFSTool(**nfsinfo)
-        self.SSHClient = ssh_tools.SSHTool(**nfsinfo)
+        self.DataStorageObj = getClsObj(datastoragenode['type'])(**datastoragenode)
+        self.LogStorageObj = getClsObj(logstoragenode['type'])(**logstoragenode)
 
         self.DependencyDict ={}
         TmpCWDPath = os.path.abspath(__file__)
@@ -58,17 +57,23 @@ class MediaGatewayTool(object):
             print ('load from file....')
             self.AppInfo = deepcopy(self.getValues())
 
-    def setupNFS(self):
-        TmpResponse = self.NFSObj.installNFS(basedir=self.AppInfo['NFSBasePath'])
+    def setupStorage(self):
+        TmpResponse = self.DataStorageObj.installStorage(basedir=self.AppInfo['DataStorageBasePath'])
         if TmpResponse['ret_code'] != 0:
             return TmpResponse
 
-        print ('create MediaGateway NFS successfully')
+
+        TmpResponse = self.LogStorageObj.installStorage(basedir=self.AppInfo['LogStorageBasePath'])
+        if TmpResponse['ret_code'] != 0:
+            return TmpResponse
+
+        print ('create MediaGateway Storage successfully')
 
 
-        self.NFSObj.createSubFolder(self.AppInfo['MediaGatewayLogPath'])
+        self.LogStorageObj.createSubFolder(self.AppInfo['MediaGatewayLogPath'])
 
-        print ('setup MediaGateway NFS successfully')
+
+        print ('setup MediaGateway Storage successfully')
 
         return {
             'ret_code': 0,
@@ -213,7 +218,7 @@ class MediaGatewayTool(object):
         }
 
     def start(self):
-        TmpResponse = self.setupNFS()
+        TmpResponse = self.setupStorage()
         if TmpResponse['ret_code'] != 0:
             return TmpResponse
 
@@ -295,30 +300,30 @@ class MediaGatewayTool(object):
             f.write('%s %s %s'%(self.AppInfo['MediaGatewayDBName'], self.AppInfo['MediaGatewayDBUser'], self.AppInfo['MediaGatewayDBPassword'])+'\n')
 
         TmpSQLToolAccountPath = '-'.join([self.AppInfo['Namespace'], 'sqltool-pv-account'])
-        TmpSQLToolAccountPath = os.path.realpath(os.path.join(self.AppInfo['NFSBasePath'], TmpSQLToolAccountPath))
+        TmpSQLToolAccountPath = os.path.realpath(os.path.join(self.AppInfo['DataStorageBasePath'], TmpSQLToolAccountPath))
         TmpSQLToolSQLPath = '-'.join([self.AppInfo['Namespace'], 'sqltool-pv-sql'])
-        TmpSQLToolSQLPath = os.path.realpath(os.path.join(self.AppInfo['NFSBasePath'], TmpSQLToolSQLPath))
+        TmpSQLToolSQLPath = os.path.realpath(os.path.join(self.AppInfo['DataStorageBasePath'], TmpSQLToolSQLPath))
 
         print (TmpSQLToolAccountPath)
         print (TmpSQLToolSQLPath)
 
-        self.SSHClient.ExecCmd('mkdir -p %s'%(TmpSQLToolSQLPath, ))
-        self.SSHClient.ExecCmd('mkdir -p %s'%(TmpSQLToolAccountPath, ))
+        self.DataStorageObj.ExecCmd('mkdir -p %s'%(TmpSQLToolSQLPath, ))
+        self.DataStorageObj.ExecCmd('mkdir -p %s'%(TmpSQLToolAccountPath, ))
 
 
 
-        self.SSHClient.ExecCmd('rm -f -r %s/*'%(TmpSQLToolAccountPath, ))
-        self.SSHClient.ExecCmd('rm -f -r %s/*'%(TmpSQLToolSQLPath,))
+        self.DataStorageObj.ExecCmd('rm -f -r %s/*'%(TmpSQLToolAccountPath, ))
+        self.DataStorageObj.ExecCmd('rm -f -r %s/*'%(TmpSQLToolSQLPath,))
 
         print (os.path.join(self.BaseDIRPath, 'tmp', 'account.txt'))
         print (os.path.join(self.BaseDIRPath, 'downloads', 'mcb_dicttool.sql'))
 
-        self.SSHClient.uploadFile(localpath=os.path.join(self.BaseDIRPath, 'tmp', 'account.txt'),
+        self.DataStorageObj.uploadFile(localpath=os.path.join(self.BaseDIRPath, 'tmp', 'account.txt'),
                                   remotepath=os.path.join(TmpSQLToolAccountPath, 'account.txt')
                                   )
 
         '''
-        self.SSHClient.uploadFile(localpath=os.path.join(self.BaseDIRPath, 'downloads', 'mcb_dicttool.sql'),
+        self.DataStorageObj.uploadFile(localpath=os.path.join(self.BaseDIRPath, 'downloads', 'mcb_dicttool.sql'),
                                   remotepath=os.path.join(TmpSQLToolSQLPath, 'mcb_dicttool.sql')
                                   )
         '''
@@ -341,12 +346,12 @@ class MediaGatewayTool(object):
 
     def postInstall(self):
         TmpNginxConfigPath = '-'.join([self.AppInfo['Namespace'], 'nginx-pv-config'])
-        TmpNginxConfigPath = os.path.realpath(os.path.join(self.AppInfo['NFSBasePath'], TmpNginxConfigPath))
+        TmpNginxConfigPath = os.path.realpath(os.path.join(self.AppInfo['DataStorageBasePath'], TmpNginxConfigPath))
 
         print (TmpNginxConfigPath)
-        self.SSHClient.ExecCmd('mkdir -p %s' % (TmpNginxConfigPath, ))
+        self.DataStorageObj.ExecCmd('mkdir -p %s' % (TmpNginxConfigPath, ))
 
-        self.SSHClient.uploadFile(localpath=os.path.join(self.BaseDIRPath, 'downloads', 'mediagateway.conf'),
+        self.DataStorageObj.uploadFile(localpath=os.path.join(self.BaseDIRPath, 'downloads', 'mediagateway.conf'),
                                   remotepath=os.path.join(TmpNginxConfigPath, 'mediagateway.conf')
                                   )
 
@@ -382,8 +387,8 @@ class MediaGatewayTool(object):
 
 
     def close(self):
-        self.NFSObj.close()
-        self.SSHClient.close()
+        self.DataStorageObj.close()
+        self.LogStorageObj.close()
 
 
 
--- apps/bigdata/mediagateway/metadata.py
+++ apps/bigdata/mediagateway/metadata.py
@@ -3,8 +3,12 @@
 
 AppInfo = {
     'AppName': 'mediagateway',
-    'NFSAddr': None,
-    'NFSBasePath': None,
+
+    'DataStorageAddr': None,
+    'DataStorageBasePath': None,
+    'LogStorageAddr': None,
+    'LogStorageBasePath': None,
+
     'MediaGatewayLogPath': None,
     'Namespace': None,
     'TargetNamespaceDIR': '../../.././namespaces',
