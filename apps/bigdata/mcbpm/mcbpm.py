fefbe79ed9bafee347c8d387633176347abf89c1
dblue <likjjdblue@gmail.com>
2021-02-28 22:22:31 +0800

    fix & update

--- apps/bigdata/mcbpm/mcbpm.py
+++ apps/bigdata/mcbpm/mcbpm.py
@@ -19,6 +19,7 @@ from codecs import open as open
 import importlib
 from storagenode import datastoragenode, logstoragenode
 from apps.storage import getClsObj
+from apps import mergeTwoDicts
 
 
 class MCBPMTool(object):
@@ -75,6 +76,10 @@ class MCBPMTool(object):
         self.LogStorageObj.createSubFolder(self.AppInfo['MCBPMLogPath'])
         self.DataStorageObj.createSubFolder(self.AppInfo['MCBPMDataPath'])
 
+        self.TmpStoragePathDict = dict()
+        self.TmpStoragePathDict['MCBPMDataPath'] = self.DataStorageObj.generateRealPath(self.AppInfo['MCBPMDataPath'])
+        self.TmpStoragePathDict['MCBPMLogPath'] = self.LogStorageObj.generateRealPath(self.AppInfo['MCBPMLogPath'])
+
         print ('setup MCBPM Storage successfully')
 
         return {
@@ -112,6 +117,9 @@ class MCBPMTool(object):
 
         if not os.path.isfile(os.path.join(TmpTargetNamespaceDIR, 'values.yaml')):
             self.generateValues()
+
+            TmpAppInfo = mergeTwoDicts(self.AppInfo, self.TmpStoragePathDict)
+
             with open(os.path.join(TmpTargetNamespaceDIR, 'values.yaml'), mode='wb') as f:
                 yaml.safe_dump(self.AppInfo, f)
 
@@ -127,7 +135,7 @@ class MCBPMTool(object):
                     TmpContent = ''
                     with open(os.path.join(basepath, file), mode='rb', encoding='utf-8') as f:
                         TmpContent = f.read()
-                    TmpContent = jinja2.Template(TmpContent).render(self.AppInfo)
+                    TmpContent = jinja2.Template(TmpContent).render(TmpAppInfo)
 
                     with open(os.path.join(basepath, file), mode='wb', encoding='utf-8') as f:
                         f.write(TmpContent)
@@ -314,13 +322,13 @@ class MCBPMTool(object):
         print (TmpSQLToolAccountPath)
         print (TmpSQLToolSQLPath)
 
-        self.DataStorageObj.ExecCmd('mkdir -p %s'%(TmpSQLToolSQLPath, ))
-        self.DataStorageObj.ExecCmd('mkdir -p %s'%(TmpSQLToolAccountPath, ))
+        self.DataStorageObj.createSubFolder(TmpSQLToolSQLPath)
+        self.DataStorageObj.createSubFolder(TmpSQLToolAccountPath)
 
 
 
-        self.DataStorageObj.ExecCmd('rm -f -r %s/*'%(TmpSQLToolAccountPath, ))
-        self.DataStorageObj.ExecCmd('rm -f -r %s/*'%(TmpSQLToolSQLPath,))
+        self.DataStorageObj.cleanSubFolder(TmpSQLToolAccountPath)
+        self.DataStorageObj.cleanSubFolder(TmpSQLToolSQLPath)
 
         print (os.path.join(self.BaseDIRPath, 'tmp', 'account.txt'))
         #print (os.path.join(self.BaseDIRPath, 'downloads', 'mcb_dicttool.sql'))
@@ -355,7 +363,7 @@ class MCBPMTool(object):
         TmpNginxConfigPath = os.path.realpath(os.path.join(self.AppInfo['DataStorageBasePath'], TmpNginxConfigPath))
 
         print (TmpNginxConfigPath)
-        self.DataStorageObj.ExecCmd('mkdir -p %s' % (TmpNginxConfigPath, ))
+        self.DataStorageObj.createSubFolder(TmpNginxConfigPath)
 
         self.DataStorageObj.uploadFile(localpath=os.path.join(self.BaseDIRPath, 'downloads', 'mcbpm.conf'),
                                   remotepath=os.path.join(TmpNginxConfigPath, 'mcbpm.conf')
