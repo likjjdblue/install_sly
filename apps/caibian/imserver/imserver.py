3b97c91a96e98be38cbed117600bbe613d44db68
dblue <likjjdblue@gmail.com>
2021-02-28 20:10:20 +0800

    fix & update

--- apps/caibian/imserver/imserver.py
+++ apps/caibian/imserver/imserver.py
@@ -19,6 +19,7 @@ from codecs import open as open
 import importlib
 from storagenode import datastoragenode, logstoragenode
 from apps.storage import getClsObj
+from apps import mergeTwoDicts
 
 
 
@@ -73,6 +74,9 @@ class IMServerTool(object):
 
         self.LogStorageObj.createSubFolder(self.AppInfo['IMServerLogPath'])
 
+        self.TmpStoragePathDict = dict()
+        self.TmpStoragePathDict['IMServerLogPath'] = self.LogStorageObj.generateRealPath(self.AppInfo['IMServerLogPath'])
+
         print ('setup IMServer Storage successfully')
 
         return {
@@ -110,6 +114,9 @@ class IMServerTool(object):
 
         if not os.path.isfile(os.path.join(TmpTargetNamespaceDIR, 'values.yaml')):
             self.generateValues()
+
+            TmpAppInfo = mergeTwoDicts(self.AppInfo, self.TmpStoragePathDict)
+
             with open(os.path.join(TmpTargetNamespaceDIR, 'values.yaml'), mode='wb') as f:
                 yaml.safe_dump(self.AppInfo, f)
 
@@ -125,7 +132,7 @@ class IMServerTool(object):
                     TmpContent = ''
                     with open(os.path.join(basepath, file), mode='rb', encoding='utf-8') as f:
                         TmpContent = f.read()
-                    TmpContent = jinja2.Template(TmpContent).render(self.AppInfo)
+                    TmpContent = jinja2.Template(TmpContent).render(TmpAppInfo)
 
                     with open(os.path.join(basepath, file), mode='wb', encoding='utf-8') as f:
                         f.write(TmpContent)
@@ -307,13 +314,13 @@ class IMServerTool(object):
         print (TmpSQLToolAccountPath)
         print (TmpSQLToolSQLPath)
 
-        self.DataStorageObj.ExecCmd('mkdir -p %s'%(TmpSQLToolSQLPath, ))
-        self.DataStorageObj.ExecCmd('mkdir -p %s'%(TmpSQLToolAccountPath, ))
+        self.DataStorageObj.createSubFolder(TmpSQLToolSQLPath)
+        self.DataStorageObj.createSubFolder(TmpSQLToolAccountPath)
 
 
 
-        self.DataStorageObj.ExecCmd('rm -f -r %s/*'%(TmpSQLToolAccountPath, ))
-        self.DataStorageObj.ExecCmd('rm -f -r %s/*'%(TmpSQLToolSQLPath,))
+        self.DataStorageObj.cleanSubFolder(TmpSQLToolAccountPath)
+        self.DataStorageObj.cleanSubFolder(TmpSQLToolSQLPath)
 
         print (os.path.join(self.BaseDIRPath, 'tmp', 'account.txt'))
         print (os.path.join(self.BaseDIRPath, 'downloads', 'mty_wcm.sql'))
@@ -349,7 +356,7 @@ class IMServerTool(object):
         TmpNginxConfigPath = os.path.realpath(os.path.join(self.AppInfo['DataStorageBasePath'], TmpNginxConfigPath))
 
         print (TmpNginxConfigPath)
-        self.DataStorageObj.ExecCmd('mkdir -p %s' % (TmpNginxConfigPath, ))
+        self.DataStorageObj.createSubFolder(TmpNginxConfigPath)
 
 
         self.DataStorageObj.uploadFile(localpath=os.path.join(self.BaseDIRPath, 'downloads', 'instantmessageservice.conf'),
