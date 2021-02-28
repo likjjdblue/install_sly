d26275fffef0c0b125954990aaaf12569021f292
dblue <likjjdblue@gmail.com>
2021-02-28 19:46:52 +0800

    fix & update

--- apps/caibian/downimg/downimg.py
+++ apps/caibian/downimg/downimg.py
@@ -19,6 +19,7 @@ from codecs import open as open
 import importlib
 from storagenode import datastoragenode, logstoragenode
 from apps.storage import getClsObj
+from apps import mergeTwoDicts
 
 
 class DownImgTool(object):
@@ -77,6 +78,10 @@ class DownImgTool(object):
         self.DataStorageObj.createSubFolder(self.AppInfo['DownImgDataPath'])
         self.LogStorageObj.createSubFolder(self.AppInfo['DownImgLogPath'])
 
+        self.TmpStoragePathDict = dict()
+        self.TmpStoragePathDict['DownImgDataPath'] = self.DataStorageObj.generateRealPath(self.AppInfo['DownImgDataPath'])
+        self.TmpStoragePathDict['DownImgLogPath'] = self.LogStorageObj.generateRealPath(self.AppInfo['DownImgLogPath'])
+
         print ('setup Down Img Storage successfully')
 
         return {
@@ -114,6 +119,9 @@ class DownImgTool(object):
 
         if not os.path.isfile(os.path.join(TmpTargetNamespaceDIR, 'values.yaml')):
             self.generateValues()
+
+            TmpAppInfo = mergeTwoDicts(self.AppInfo, self.TmpStoragePathDict)
+
             with open(os.path.join(TmpTargetNamespaceDIR, 'values.yaml'), mode='wb') as f:
                 yaml.safe_dump(self.AppInfo, f)
 
@@ -129,7 +137,7 @@ class DownImgTool(object):
                     TmpContent = ''
                     with open(os.path.join(basepath, file), mode='rb', encoding='utf-8') as f:
                         TmpContent = f.read()
-                    TmpContent = jinja2.Template(TmpContent).render(self.AppInfo)
+                    TmpContent = jinja2.Template(TmpContent).render(TmpAppInfo)
 
                     with open(os.path.join(basepath, file), mode='wb', encoding='utf-8') as f:
                         f.write(TmpContent)
@@ -310,13 +318,13 @@ class DownImgTool(object):
         print (TmpSQLToolAccountPath)
         print (TmpSQLToolSQLPath)
 
-        self.DataStorageObj.ExecCmd('mkdir -p %s'%(TmpSQLToolSQLPath, ))
-        self.DataStorageObj.ExecCmd('mkdir -p %s'%(TmpSQLToolAccountPath, ))
+        self.DataStorageObj.createSubFolder(TmpSQLToolSQLPath)
+        self.DataStorageObj.createSubFolder(TmpSQLToolAccountPath)
 
 
 
-        self.DataStorageObj.ExecCmd('rm -f -r %s/*'%(TmpSQLToolAccountPath, ))
-        self.DataStorageObj.ExecCmd('rm -f -r %s/*'%(TmpSQLToolSQLPath,))
+        self.DataStorageObj.cleanSubFolder(TmpSQLToolAccountPath)
+        self.DataStorageObj.cleanSubFolder(TmpSQLToolSQLPath)
 
         '''print (os.path.join(self.BaseDIRPath, 'tmp', 'account.txt'))
         print (os.path.join(self.BaseDIRPath, 'downloads', 'mty_wcm.sql'))'''
@@ -350,7 +358,7 @@ class DownImgTool(object):
         TmpNginxConfigPath = os.path.realpath(os.path.join(self.AppInfo['DataStorageBasePath'], TmpNginxConfigPath))
 
         print (TmpNginxConfigPath)
-        self.DataStorageObj.ExecCmd('mkdir -p %s' % (TmpNginxConfigPath, ))
+        self.DataStorageObj.createSubFolder(TmpNginxConfigPath)
 
         self.DataStorageObj.uploadFile(localpath=os.path.join(self.BaseDIRPath, 'downloads', 'tmyimgcenter.conf'),
                                   remotepath=os.path.join(TmpNginxConfigPath, 'tmyimgcenter.conf')
