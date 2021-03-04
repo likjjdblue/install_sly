73fb05893d228cc01b8ee321faf3dca128bd569e
dblue <likjjdblue@gmail.com>
2021-03-04 10:13:42 +0800

    fix & update;NFS 动态支持指定单独存储节点

--- storagenode_example_template/storagenode_nas.py
+++ storagenode_example_template/storagenode_nas.py
@@ -35,4 +35,20 @@ logstoragenode = {
     'baseurl': '/volume1/NFS2'
 }
 
+
+######         dynamicstoragenode   配置项说明      ###
+# NAS 数据存储节点，用于存放中间件数据（Redis,MongoDB,Elasticsearch,Kafka,Zookeeper,Nacos,Rabbitmq）；提供的NAS 只能是以NFS 方式访问（块存储方式，目前不支持）
+# hostname: NAS 访问IP 地址 (必填项，按实际情况修改)
+# baseurl: NAS 访问URL （必填项，按实际情况修改）
+# basepath:  程序内部地址   （非必填项，不建议修改）
+######      END      ###########
+
+dynamicstoragenode = {
+    'type': 'nas',
+    'hostname':  '192.168.200.74',
+    'basepath': '/TRS/DATA',
+    'baseurl': '/home/tmp_nfs_storage'
+}
+
+
 namespace = 'sly2'
--- storagenode_example_template/storagenode_nfs.py
+++ storagenode_example_template/storagenode_nfs.py
@@ -48,4 +48,27 @@ logstoragenode = {
 
 
 
+######         dynamicstoragenode   配置项说明      ###
+#  NFS 数据存储节点，用于存放中间件数据（Redis,MongoDB,Elasticsearch,Kafka,Zookeeper,Nacos,Rabbitmq）；提供的节点信息必须能够SSH 可达
+# hostname: NFS 服务器 SSH IP 地址 (必填项，按实际情况修改)
+# port:  NFS 服务器 SSH 端口 (必填项； SSH 端口默认是 22，如果非默认端口，按实际情况修改)
+# username: NFS 服务器 SSH 用户名 (必填项； 工具运行过程中需要使用ROOT 权限，请使用ROOT 账号)
+# password：  NFS 服务器 SSH root 账号密码 (必填项； 工具运行过程中需要使用ROOT 权限，请使用ROOT 账号密码)
+#  baseurl:   NFS 服务挂载路径  （非必填项，不建议修改）
+#   basepath:   非必填项，不建议修改
+######      END      ###########
+
+
+dynamicstoragenode = {
+    'type': 'nfs',
+    'hostname':  '192.168.200.74',
+    'port': 22,
+    'username': 'root',
+    'password': 'TempPass',
+    'basepath': '/',
+    'baseurl': '/TRS/DATA',
+}
+
+
+
 namespace = 'sly2'
